421de8a8d034ff82728bb9b10d28d2e96e28004e
Clarissa Medina <clariimedina@gmail.com>
2014-12-06 20:32:59 -0700

    actualizacion de vistas
    nuevas funcionalidades

--- cartelera.php
+++ cartelera.php
@@ -1,4 +1,4 @@
--<?php include("header.php"); ?>
+<?php include("header.php"); ?>
 <div id="main">
 	<div class="cartelera-contenedor">
 
@@ -24,17 +24,18 @@ $(document).ready(function(response) {
 						codigo+="<div class=\"cartelera-icono\">";
 						codigo+="<i class=\"fa fa-map-marker\"></i>";
 						codigo+="</div>";
-						codigo+="<h4>"+complejos.nombre+" "+complejos.complejo+"</h4>";
+						codigo+="<h4>"+complejos.nombre+"</h4>";
 						codigo+="</div>";
 						codigo+="<div class=\"cartelera-peliculas\">";
-						codigo+="<ul>";
-						codigo+="<li><a id=\"1\" href=\"pelicula.php\"><img src=\"images/poster/poster1.jpg\"></a><br><center>Los juegos del hambre</center></li>";
-						codigo+="<li><a id=\"2\"><img src=\"images/poster/poster2.jpg\"></a><br><center>Annabelle</center></li>";
-						codigo+="<li><a id=\"3\"><img src=\"images/poster/poster3.png\"></a><br><center>Interestelar</center></li>";
-						codigo+="<li><a id=\"4\"><img src=\"images/poster/poster4.jpg\"></a><br><center>Una pareja mas tonta</center></li>";
-						codigo+="<li><a id=\"5\"><img src=\"images/poster/poster5.jpg\"></a><br><center>La dictadura perfecta</center></li>";
-						codigo+="</ul>"; 
-						codigo+="</div>";
+						codigo+="<ul id=\"PeliculasporComplejo\">";
+                    		$.each(complejos.peliculas, function(index,peliculas) { // function(Index,ganado)
+
+								codigo+="<li><a id="+peliculas.id_pelicula+" href=\"peliculaInfo.php?id_pelicula="+peliculas.id_pelicula+"\"><img src=\"images/poster/"+peliculas.poster+"\"></a><br><center>"+peliculas.titulo+"</center></li>";
+                		
+                  			   });
+                    			$('#PeliculasporComplejo').html(codigo); 
+					codigo+="</ul>"; 
+					codigo+="</div>";
                     		
                    });
                     $('#contenido').html(codigo);
@@ -45,4 +46,5 @@ $(document).ready(function(response) {
 });
 
 </script>
+
 <?php include("footer.php"); ?>
---
+++ peliculaInfo.php
@@ -0,0 +1,109 @@
+<?php include("header.php"); ?>
+<div id="main">
+</div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+     var id = getParameterByName('id_pelicula');
+     $.ajax({
+           url: "http://localhost/cineBack/index.php/api/Peliculas?id="+id, // URL del recurso
+            type: "GET",         
+            dataType: "json",
+            success: function(response) {
+                var codigo='';
+                if (response.success == true) {
+                       codigo+="<div class=\"pelicula-contenedor\">";
+                       codigo+="<div class=\"pelicula\">";
+                       codigo+="<img src=\"images/poster/"+response.peliInfo.poster+"\">";
+                       codigo+="<div class=\"pelicula-info\">";
+                       codigo+="<h5>"+response.peliInfo.titulo+"</h5><hr>";
+                       codigo+="<ul>";
+                       codigo+="<li><strong>Titulo original:</strong>"+response.peliInfo.titulo+"</li>";
+                       codigo+="<li><strong>Actores:</strong>"+response.peliInfo.actores+"</li>";
+                       codigo+="<li><strong>Director:</strong>"+response.peliInfo.director+"</li>";
+                       codigo+="<li><strong>Pais:</strong>"+response.peliInfo.pais+"</li>";
+                       codigo+="<li><strong>Año:</strong>"+response.peliInfo.año+"</li>";
+                       codigo+="<li><strong>Clasificación:</strong>"+response.peliInfo.clasificacion+"</li>";
+                       codigo+="<li><strong>Duración:</strong>"+response.peliInfo.duracion+"</li>";
+                       codigo+="<li><strong>Género</strong>"+response.peliInfo.genero+"</li>";
+                       codigo+="</ul>";
+                       codigo+="</div>";
+                       codigo+="</div>";
+                       codigo+="</div>";
+                       codigo+="<div class=\"horarios-contenedor\">";
+                       codigo+="<div id=\"horario-instruccion\">";
+                       codigo+="<span>Selecciona un horario para comprar y/o reservar</span>";
+                       codigo+="</div>";
+                       codigo+="<div class=\"fecha-seleccion\">";
+                       codigo+="<span><center>Selecciona la fecha</center></span>";
+                       codigo+="<select id=\"fecha\" class=\"form-control\">";
+                       codigo+="<option>Miercoles 26 de Noviembre</option>";
+                       codigo+="<option>Miercoles 27 de Noviembre</option>";
+                       codigo+="<option>Miercoles 28 de Noviembre</option>";
+                       codigo+="</select>";
+                       codigo+="</div>";
+                       codigo+="<div class=\"horarios\">";
+                       codigo+="<div class=\"seleccion-sala\">";
+                       codigo+="<span>Digital </span><span>Español</span> <i class=\"fa fa-clock-o\"></i>";
+                       codigo+="</div>";
+                       codigo+="<div class=\"seleccion-hora\">";
+                       codigo+="<ul id=\"lista\">";
+                       codigo+="<li><a href=\"compraBoletos.php?horario=11:00&pelicula=sinsajo&numSala=1&tipoSala=digitalEspañol\" title=\"comprar boletos\">11:00 AM</a></li>";
+                       codigo+="<li><a>11:45 AM</a></li>";
+                       codigo+="<li><a>12:45 AM</a></li>";
+                       codigo+="<li><a>1:35 AM</a></li>";
+                       codigo+="<li><a>2:15 AM</a></li>";
+                       codigo+="<li><a>2:15 AM</a></li>";
+                       codigo+="<li><a>2:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="</ul>";
+                       codigo+="</div>";
+                       codigo+="<div class=\"seleccion-sala\"><span>Digital </span><SPAN>Ingles</SPAN></div>";
+                       codigo+="<div class=\"seleccion-hora\">";
+                       codigo+="<ul id=\"lista\">";
+                       codigo+="<li><a href=\"compraBoletos.php?horario=11:00&pelicula=sinsajo&numSala=1&tipoSala=digitalEspañol\" title=\"comprar boletos\">11:00 AM</a></li>";
+                       codigo+="<li><a>11:45 AM</a></li>";
+                       codigo+="<li><a>12:45 AM</a></li>";
+                       codigo+="<li><a>1:35 AM</a></li>";
+                       codigo+="<li><a>2:15 AM</a></li>";
+                       codigo+="<li><a>2:15 AM</a></li>";
+                       codigo+="<li><a>2:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="<li><a>3:15 AM</a></li>";
+                       codigo+="</ul>";
+                       codigo+="</div>";
+                       codigo+="</div>";
+                       codigo+="</div>";
+                       codigo+="<div class=\"info-pelicula-contenedor\">";
+                       codigo+="<div class=\"info-pelicula-opciones\">";
+                       codigo+="<ul>";
+                       codigo+="<li id=\"trailer-link\" class=\"info-pelicula-enlaces-select\"><a id=\"trailer\" class=\"info-pelicula-opciones-select\"><i class=\"fa fa-play\" style=\"margin-right:10px;\"></i>Trailer</a></li>";
+                       codigo+="<li id=\"sipnosis-link\" class=\"info-pelicula-enlaces\"><a id=\"sipnosis\" class=\"info-pelicula-opciones\"><i class=\"fa fa-align-left\"></i> Sipnosis</a></li>";
+                       codigo+="</ul>";
+                       codigo+="</div>";
+                       codigo+="<div class=\"info-pelicula-contenido\">";
+                       codigo+="<div class=\"info-pelicula-detalles-select\" id=\"contenido-trailer\">";
+                       codigo+="<iframe width=\"700\" height=\"400\" src=\"http://www.youtube.com/embed/ryd-GcG6Hkk\"></iframe>";
+                       codigo+="<div class=\"info-pelicula-detalles\" id=\"contenido-sipnosis\">";
+                       codigo+="<p>Los juegos del hambre: Sinsajo Parte 1 en la que Katniss Everdeen se encuentra en el Distrito 13 después de destrozar literalmente los Juegos para siempre. Bajo el liderazgo de la Presidente Coin y el consejo de sus amigos más leales, Katniss extiende sus alas mientras lucha por salvar a Peeta Mellark y a una nación alentada por su valentía.</p>";
+                       codigo+="</div>";
+                       codigo+="</div>";
+                       codigo+="</div>";
+                       codigo+="</div>";
+                    $('#main').html(codigo);
+               }
+            }   
+     }); 
+});
+
+</script>
+<?php include('footer.php');?>
